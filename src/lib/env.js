1e96ceff4e41f80e7283110b06c9d4a99ef15660
Brian Leppez <brian.leppez@upskill.io>
2018-07-11 12:37:22 -0500

    upgraded yenv to 2.1

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,14 +1,11 @@
 import yenv from 'yenv'
-import { keyblade } from 'keyblade'
 import { logger } from './logger'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-/**
- * We just export what `yenv()` returns.
- * `keyblade` will make sure we don't rely on undefined values.
- */
-export const env = keyblade(yenv(), {
-  message: key => `[yenv] ${key} not found in the loaded environment`,
-  logBeforeThrow: message => logger.error(message)
-})
+// We just export what `yenv()` returns.
+export const env = yenv('env.yaml', {
+    message: key => `[yenv] ${key} not found in the loaded environment`,
+    logBeforeThrow: message => logger.error(message)
+});
+
