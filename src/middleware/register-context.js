ae90a72316292e86f94568dd94a0945163640c1f
Ray <i@iraycd.com>
2018-12-18 15:21:22 +0530

    regular async function and uses docblock style comments in middleware

--- src/middleware/register-context.js
+++ src/middleware/register-context.js
@@ -1,8 +1,10 @@
 import { asValue } from 'awilix'
 
-// Register Context helps to add request-specific data to the scope.
-export const registerContext = async (ctx, next) => {
-  // Imagine some auth middleware somewhere...
+/**
+ * Register Context helps to add request-specific data to the scope.
+ * Imagine some auth middleware somewhere...
+ */
+export async function registerContext(ctx, next) {
   ctx.state.container.register({
     userContext: asValue({
       user: 'username'
